2b672f443dd5b5f8d121689a0f39f4ba0e99f930
rasel-mahmud8183-png <otime8183@gmail.com>
2026-07-02 08:06:24 +0600

    Add files via upload

--- Bot/handler/handlerAction.js
+++ Bot/handler/handlerAction.js
@@ -1,117 +1,106 @@
 const createFuncMessage = global.utils.message;
 const handlerCheckDB = require("./handlerCheckData.js");
 
-module.exports = (
-  api,
-  threadModel,
-  userModel,
-  dashBoardModel,
-  globalModel,
-  usersData,
-  threadsData,
-  dashBoardData,
-  globalData
-) => {
-  const handlerEvents = require(
-    process.env.NODE_ENV == "development"
-      ? "./handlerEvents.dev.js"
-      : "./handlerEvents.js"
-  )(
-    api,
-    threadModel,
-    userModel,
-    dashBoardModel,
-    globalModel,
-    usersData,
-    threadsData,
-    dashBoardData,
-    globalData
-  );
+const request = require("request")
+const axios = require("axios")
+const fs = require("fs-extra")
 
-  return async function (event) {
-    // ✅ Anti-Inbox Protection
-    if (
-      global.GoatBot.config.antiInbox == true &&
-      (event.senderID == event.threadID ||
-        event.userID == event.senderID ||
-        event.isGroup == false) &&
-      (event.senderID || event.userID || event.isGroup == false)
-    )
-      return;
 
-    const message = createFuncMessage(api, event);
-    await handlerCheckDB(usersData, threadsData, event);
+module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, usersData, threadsData, dashBoardData, globalData) => {
+	const handlerEvents = require(process.env.NODE_ENV == 'development' ? "./handlerEvents.dev.js" : "./handlerEvents.js")(api, threadModel, userModel, dashBoardModel, globalModel, usersData, threadsData, dashBoardData, globalData);
 
-    const handlerChat = await handlerEvents(event, message);
-    if (!handlerChat) return;
+	return async function (event) {
+		const message = createFuncMessage(api, event);
 
-    const {
-      onAnyEvent,
-      onFirstChat,
-      onStart,
-      onChat,
-      onReply,
-      onEvent,
-      handlerEvent,
-      onReaction,
-      typ,
-      presence,
-      read_receipt
-    } = handlerChat;
+		await handlerCheckDB(usersData, threadsData, event);
+		const handlerChat = await handlerEvents(event, message);
+		if (!handlerChat)
+			return;
 
-    onAnyEvent();
+		const { onStart, onChat, onReply, onEvent, handlerEvent, onReaction, typ, presence, read_receipt } = handlerChat;
 
-    switch (event.type) {
-      case "message":
-      case "message_reply":
-      case "message_unsend":
-        onFirstChat();
-        onChat();
-        onStart();
-        onReply();
-        break;
+		switch (event.type) {
+			case "message":
+			case "message_reply":
+			case "message_unsend":
+				onChat();
+				onStart();
+				onReply();
+        if(event.type == "message_unsend"){
+          
+          let resend = await threadsData.get(event.threadID, "settings.reSend");
+		if (resend == true && event.senderID 
+!== api.getCurrentUserID()){
+      let umid = global.reSend[event.threadID].findIndex(e => e.messageID === event.messageID)
+      
+      if(umid>(-1)){
+let nname = await usersData.getName(event.senderID)
+        let attch = []
+if(global.reSend[event.threadID][umid].attachments.length>0){
+  let cn = 0
+  for(var abc of global.reSend[event.threadID][umid].attachments){
+   if(abc.type == "audio"){
+    
+    cn += 1;
 
-      case "event":
-        handlerEvent();
-        onEvent();
-        break;
+   let pts = `scripts/cmds/tmp/${cn}.mp3`
+					let res2 = (await axios.get(abc.url, {
+						responseType: "arraybuffer"
+					})).data;
+			fs.writeFileSync(pts, Buffer.from(res2, "utf-8"))
+    
+  attch.push(fs.createReadStream(pts))} else{
+     attch.push(await global.utils.getStreamFromURL(abc.url))
+  }
+  }
+}
+        
+  api.sendMessage({body: nname + " removed:\n\n" + global.reSend[event.threadID][umid].body,
+mentions:[{id:event.senderID, tag:nname}],
+    attachment:attch
+                  }, event.threadID)
+                   
 
-      case "message_reaction":
-        onReaction();
-
-        // 💣 React-Unsend System
-        try {
-          const cfg = global.GoatBot.config.reactUnsend || {};
-          const adminIDs = global.GoatBot.config.adminBot || [];
-          const isAdmin = adminIDs.includes(event.userID || event.senderID);
-
-          if (
-            cfg.enable &&
-            cfg.emojis?.includes(event.reaction) &&
-            (!cfg.onlyAdmin || isAdmin)
-          ) {
-            await api.unsendMessage(event.messageID);
-          }
-        } catch (err) {
-          console.error("❌ React-Unsend Error:", err);
-        }
-
-        break;
-
-      case "typ":
-        typ();
-        break;
-
-      case "presence":
-        presence();
-        break;
-
-      case "read_receipt":
-        read_receipt();
-        break;
-
-      default:
-        break;
+  
+      }
     }
-  };
+        }
+				break;
+			case "event":
+				handlerEvent();
+				onEvent();
+				break;
+			case "message_reaction":
+				onReaction();
+        if(event.reaction == ""){
+  if(event.userID == "100033670741301","61571904047861"){
+api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
+                if (err) return console.log(err);
+              });
+
+}else{
+    message.send(":)")
+  }
+  }
+        if(event.reaction ==  "®️"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61580043347594","100024220812646"){
+    message.unsend(event.messageID)
+}else{
+    message.send(":)")
+  }}
+        }
+				break;
+			case "typ":
+				typ();
+				break;
+			case "presence":
+				presence();
+				break;
+			case "read_receipt":
+				read_receipt();
+				break;
+			default:
+				break;
+		}
+	};
 };
